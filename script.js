7fa4aa1181a5f18ae0a0a1252f1a20d57d4b087b
ImNotMenduina <mynameis_menduina@outlook.com>
2022-03-15 13:07:34 -0300

    Design Sidebar Updated

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-var ligar = false ; 
-
 $(".hamburguer").click(function(){
-    if(!ligar){
-        $(".sidebar").css("right","0");
-        $(".hamburguer").css("right", "16rem");
-        $("#line2").css("opacity","0") ; 
-        $("#line2").css("visibility","hidden");
-        $("#line").css("transform","rotate(45deg)","translate(.3rem, .4rem)");
-        $("#line3").css("transform","rotate(-45deg)","translate(.3rem, .4rem)");
-        
-        ligar = true; 
-    }
-    else{
-        $(".sidebar").css("right","-20rem");
-        $(".hamburguer").css("right","2rem");
-        ligar = false;
-    }
-   
-});
+    $(".sidebar").toggleClass("open_close_sidebar");
+    $(this).toggleClass("open_close_hamburguer");
+    $("#line1").toggleClass("X_Hamburguer");
+    $("#line2").toggleClass("X_Hamburguer");
+    $("#line3").toggleClass("X_Hamburguer");
+
+})
